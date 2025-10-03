a51e246245e6337516f3a65951b57efeae916ee9
Angel Vilar Sanchez <angelvilarmacpro@MacBook-Pro-de-Angel.local>
2025-10-03 09:28:56 +0200

    Simplificado start.sh (sin -m, que daba error)

--- start.sh
+++ start.sh
@@ -3,11 +3,7 @@ set -euo pipefail
 
 echo "[start.sh] Arrancando Rasa con PORT=${PORT:-5005}…"
 
-# Selecciona el último modelo entrenado
-MODEL_PATH=$(ls -t models/*.tar.gz | head -n 1)
-
 exec rasa run \
-  -m "${MODEL_PATH}" \
   --enable-api \
   --cors "*" \
   --host 0.0.0.0 \
