899ad69111808182f2cc88ca8995395403f0a52d
Angel Vilar Sanchez <angelvilarmacpro@MacBook-Pro-de-Angel.local>
2025-10-06 12:22:47 +0200

    Add executable start.sh for Render deployment

--- start.sh
+++ start.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-echo "[start.sh] Arrancando Rasa con PORT=${PORT-5005}…"
+echo "[start.sh] Arrancando Rasa con PORT=${PORT:-5005}..."
 
-rasa run --enable-api --cors "*" --host "0.0.0.0" --port ${PORT-5005}
+exec python -m rasa run \
+  --enable-api \
+  --cors "*" \
+  --host 0.0.0.0 \
+  --port ${PORT:-5005}
+
