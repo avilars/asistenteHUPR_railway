9acd2c788e3913f7b5a378731edd7692b527febc
Angel Vilar Sanchez <angelvilarmacpro@MacBook-Pro-de-Angel.local>
2025-10-06 11:57:03 +0200

    fix start.sh final

--- start.sh
+++ start.sh
@@ -1,11 +1,4 @@
-#!/usr/bin/env bash
-set -euo pipefail
+#!/bin/bash
+echo "[start.sh] Arrancando Rasa con PORT=${PORT-5005}…"
 
-echo "[start.sh] Arrancando Rasa con PORT=${PORT:-5005}…"
-
-exec rasa run \
-  --enable-api \
-  --cors "*" \
-  --host "0.0.0.0" \
-  --port "${PORT:-5005}" \
-  --credentials "credentials.yml"
+rasa run --enable-api --cors "*" --host "0.0.0.0" --port ${PORT-5005}
