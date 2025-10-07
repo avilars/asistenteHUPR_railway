c56baeed020b1b020affa23ff74051612f5f6984
Angel Vilar Sanchez <angelvilarmacpro@MacBook-Pro-de-Angel.local>
2025-10-07 17:21:31 +0200

    corrige host en start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,3 @@
-#!/usr/bin/env bash
-set -Eeuo pipefail
+#!/bin/bash
 echo "[start.sh] Arrancando Rasa con PORT=${PORT:-5005}..."
-
-# Activa el entorno virtual si existe
-if [[ -d ".venv" ]]; then
-  source .venv/bin/activate
-fi
-
-# Ejecuta el entrypoint correcto
-exec python -m rasa.__main__ run --enable-api --cors "*" --host 0.0.0.0 --port "${PORT:-5005}"
+rasa run --enable-api --cors "*" --host "0.0.0.0" --port ${PORT:-5005} -m models --credentials credentials.yml
