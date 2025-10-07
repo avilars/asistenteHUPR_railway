1264e035c7620d1117dc1ac4d9d8bfb9eb9038ab
Angel Vilar Sanchez <angelvilarmacpro@MacBook-Pro-de-Angel.local>
2025-10-07 17:32:07 +0200

    corrige ejecución Rasa en Render

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
-echo "[start.sh] Arrancando Rasa con PORT=${PORT:-5005}..."
-rasa run --enable-api --cors "*" --host "0.0.0.0" --port ${PORT:-5005} -m models --credentials credentials.yml
+#!/usr/bin/env bash
+echo "[start.sh] Iniciando Rasa con PORT=${PORT:-5005} ..."
+exec python3 -m rasa run --enable-api --cors "*" --host 0.0.0.0 --port ${PORT:-5005} -m models --credentials credentials.yml
