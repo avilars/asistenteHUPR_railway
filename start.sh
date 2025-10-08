e5af5953992694ccdfde768bf76347516ed1f6ce
Angel Vilar Sanchez <angelvilarmacpro@MacBook-Pro-de-Angel.local>
2025-10-08 06:53:27 +0200

    corrige permisos start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/env bash
-echo "[start.sh] Iniciando Rasa con PORT=${PORT:-5005} ..."
-exec python3 -m rasa run --enable-api --cors "*" --host 0.0.0.0 --port ${PORT:-5005} -m models --credentials credentials.yml
+#!/bin/bash
+echo "[start.sh] Arrancando Rasa con PORT=${PORT}..."
+rasa run --enable-api --cors "*" --host 0.0.0.0 --port ${PORT:-5005} -m models/$(ls -t models | head -n1) --credentials credentials.yml --endpoints endpoints.yml
