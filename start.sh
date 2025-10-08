df5936dfbb3446d57cf69e9570dfff9ab0d6f4f3
Angel Vilar Sanchez <angelvilarmacpro@MacBook-Pro-de-Angel.local>
2025-10-08 17:12:43 +0200

    Corrige start.sh para Railway

--- start.sh
+++ start.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 echo "[start.sh] Arrancando Rasa con PORT=${PORT}..."
-rasa run --enable-api --cors "*" --host 0.0.0.0 --port ${PORT:-5005} -m models/$(ls -t models | head -n1) --credentials credentials.yml --endpoints endpoints.yml
+rasa run --enable-api --cors "*" --host 0.0.0.0 --port ${PORT:-5005} \
+  -m models/$(ls -t models | head -n1) \
+  --credentials credentials.yml \
+  --endpoints endpoints.yml
