677c71768f64393387a04d4dc5d1b8fc03e9bb46
Angel Vilar Sanchez <angelvilarmacpro@MacBook-Pro-de-Angel.local>
2025-10-08 22:52:49 +0200

    Revertir a versión estable de start.sh

--- start.sh
+++ start.sh
@@ -6,5 +6,5 @@ ls models
 
 # Iniciar el servidor Rasa con el modelo ya entrenado
 rasa run --enable-api --cors "*" --host 0.0.0.0 --port ${PORT:-5005} \
-  -m models/20251002-142542-similar-delta.tar.gz \
+  -m models/20251008-214320-burgundy-band.tar.gz \
   --credentials credentials.yml --endpoints endpoints.yml
