35be3119907745a931859a1d9f00dba829964e41
Angel Vilar Sanchez <angelvilarmacpro@MacBook-Pro-de-Angel.local>
2025-10-08 22:59:59 +0200

    Revertir a start.sh estable probado

--- start.sh
+++ start.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 echo "🚀 Iniciando Asistente HUPR en Railway..."
 
-# Mostrar el contenido del directorio de modelos
+# Mostrar contenido del directorio models
 ls models
 
-# Iniciar el servidor Rasa con el modelo ya entrenado
-rasa run --enable-api --cors "*" --host 0.0.0.0 --port ${PORT:-5005} \
-  -m models/20251008-214320-burgundy-band.tar.gz \
-  --credentials credentials.yml --endpoints endpoints.yml
+# Ejecutar servidor Rasa
+rasa run -m models/20251002-142542-similar-delta.tar.gz --enable-api --cors "*" --host 0.0.0.0 --port ${PORT:-8080} --credentials credentials.yml --endpoints endpoints.yml
