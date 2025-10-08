81e332a046de7fa662412c325b0d259365e4e1d7
Angel Vilar Sanchez <angelvilarmacpro@MacBook-Pro-de-Angel.local>
2025-10-08 23:01:53 +0200

    Restaurar start.sh estable (sin saltos de línea)

--- start.sh
+++ start.sh
@@ -5,4 +5,4 @@ echo "🚀 Iniciando Asistente HUPR en Railway..."
 ls models
 
 # Ejecutar servidor Rasa
-rasa run -m models/20251002-142542-similar-delta.tar.gz --enable-api --cors "*" --host 0.0.0.0 --port ${PORT:-8080} --credentials credentials.yml --endpoints endpoints.yml
+rasa run -m models/20251008-214320-burgundy-band.tar.gz --enable-api --cors "*" --host 0.0.0.0 --port ${PORT:-8080} --credentials credentials.yml --endpoints endpoints.yml
