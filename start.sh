6a4bf1a80d2c9678af8110fcc08d5f0e1fc3eb83
Angel Vilar Sanchez <angelvilarmacpro@MacBook-Pro-de-Angel.local>
2025-10-08 17:29:10 +0200

    elimina start.sh para usar comando directo en Render

--- start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "[start.sh] Arrancando Rasa con PORT=${PORT}..."
-rasa run --enable-api --cors "*" --host 0.0.0.0 --port ${PORT:-5005} \
-  -m models/$(ls -t models | head -n1) \
-  --credentials credentials.yml \
-  --endpoints endpoints.yml
