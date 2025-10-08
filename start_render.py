c562153cec745b6c8d4d75dac0946b606aabe932
Angel Vilar Sanchez <angelvilarmacpro@MacBook-Pro-de-Angel.local>
2025-10-08 17:47:30 +0200

    Ejecuta Rasa como módulo interno para Render

--- start_render.py
+++ start_render.py
@@ -1,25 +1,17 @@
 import os
-import subprocess
+from rasa.__main__ import main as rasa_main
 
-port = os.getenv("PORT", "5005")
-print(f"[start_render.py] Arrancando Rasa en puerto {port}...")
-
-cmd = [
-    "rasa",
-    "run",
-    "--enable-api",
-    "--cors",
-    "*",
-    "--host",
-    "0.0.0.0",
-    "--port",
-    port,
-    "-m",
-    "models",
-    "--credentials",
-    "credentials.yml",
-    "--endpoints",
-    "endpoints.yml",
-]
-
-subprocess.run(cmd)
+if __name__ == "__main__":
+    port = os.getenv("PORT", "5005")
+    print(f"[start_render.py] Arrancando Rasa en puerto {port}...")
+    args = [
+        "run",
+        "--enable-api",
+        "--cors", "*",
+        "--host", "0.0.0.0",
+        "--port", port,
+        "-m", "models",
+        "--credentials", "credentials.yml",
+        "--endpoints", "endpoints.yml"
+    ]
+    rasa_main(args)
