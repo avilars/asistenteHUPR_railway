0d023fdf059af16cca4eaf83bbbe5ac2fc078e1a
Angel Vilar Sanchez <angelvilarmacpro@MacBook-Pro-de-Angel.local>
2025-10-08 20:43:54 +0200

    Corrige arranque de Rasa en Render (fix puerto)

--- start_render.py
+++ start_render.py
@@ -3,19 +3,13 @@ import subprocess
 
 print("[start_render.py] Arrancando Rasa en puerto 5005...")
 
-# Asegurar puerto
-port = os.getenv("PORT", "5005")
-
-# Comando completo de ejecución
-command = [
+subprocess.run([
     "rasa", "run",
     "--enable-api",
     "--cors", "*",
     "--host", "0.0.0.0",
-    "--port", port,
+    "--port", "5005",
+    "-m", f"models/{sorted(os.listdir('models'))[-1]}",
     "--credentials", "credentials.yml",
     "--endpoints", "endpoints.yml"
-]
-
-# Ejecutar y mostrar salida en tiempo real
-subprocess.run(command)
+])
