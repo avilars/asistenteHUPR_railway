e9fc79ed8144508223044c25cc299a41263ab812
Angel Vilar Sanchez <angelvilarmacpro@MacBook-Pro-de-Angel.local>
2025-10-08 18:00:29 +0200

    Corrige arranque Rasa por subprocess.run

--- start_render.py
+++ start_render.py
@@ -1,17 +1,21 @@
 import os
-from rasa.__main__ import main as rasa_main
+import subprocess
 
-if __name__ == "__main__":
-    port = os.getenv("PORT", "5005")
-    print(f"[start_render.py] Arrancando Rasa en puerto {port}...")
-    args = [
-        "run",
-        "--enable-api",
-        "--cors", "*",
-        "--host", "0.0.0.0",
-        "--port", port,
-        "-m", "models",
-        "--credentials", "credentials.yml",
-        "--endpoints", "endpoints.yml"
-    ]
-    rasa_main(args)
+print("[start_render.py] Arrancando Rasa en puerto 5005...")
+
+# Asegurar puerto
+port = os.getenv("PORT", "5005")
+
+# Comando completo de ejecución
+command = [
+    "rasa", "run",
+    "--enable-api",
+    "--cors", "*",
+    "--host", "0.0.0.0",
+    "--port", port,
+    "--credentials", "credentials.yml",
+    "--endpoints", "endpoints.yml"
+]
+
+# Ejecutar y mostrar salida en tiempo real
+subprocess.run(command)
