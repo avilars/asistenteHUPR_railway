673bd477840f62c1f9d604abd673b6ac1146871f
Angel Vilar Sanchez <angelvilarmacpro@MacBook-Pro-de-Angel.local>
2025-10-08 21:46:26 +0200

    Corrige parámetro host (-i 0.0.0.0) para Railway

--- start.sh
+++ start.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 echo "🚀 Iniciando Asistente HUPR en Railway..."
-rasa run --enable-api --cors "*" --host 0.0.0.0 --port ${PORT:-5005} -m models/$(ls -t models | head -1) --credentials credentials.yml --endpoints endpoints.yml
+rasa run --enable-api --cors "*" -i 0.0.0.0 -p ${PORT:-5005} \
+  -m models/$(ls -t models | head -1) \
+  --credentials credentials.yml \
+  --endpoints endpoints.yml
